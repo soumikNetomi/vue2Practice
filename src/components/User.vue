56deb1ed4f69c434a8e78ecacf0dd471aa553649
soumik <ankit.singh.81@gmail.com>
2023-09-25 15:21:09 +0530

    create user Component

---
+++ src/components/User.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="userContainer">
+        <input type="text" placeholder="Enter Fruit Name"><br>
+        <input type="number" placeholder="Enter Quantity"><br>
+        <button class="submit">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "User"
+}
+</script>
+
+<style scoped>
+input {
+    margin-bottom: 20px;
+    padding: 5px 10px;
+    font-size: 14px;
+}
+
+input:focus {
+    outline: none;
+}
+
+.submit {
+    background-color: lightgreen;
+    outline: none;
+    border: none;
+    padding: 7px 30px;
+    border-radius: 20px;
+}
+
+.userContainer {
+    text-align: center;
+    margin-top: 60px;
+    padding: 20px;
+    border: 1px solid red;
+}</style>
